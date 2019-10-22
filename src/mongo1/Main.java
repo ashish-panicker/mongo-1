b96f41826907d264707885f0e3346f77f9d8dd3e
ashish-panicker <ashish.s.panicker@gmail.com>
2019-10-22 12:28:22 +0530

    mongo 1 with create, remove, read,upddate and connection manager

--- src/mongo1/Main.java
+++ src/mongo1/Main.java
@@ -13,14 +13,15 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Updates;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
-		MongoDatabase database = mongoClient.getDatabase("test");
+		MongoClient mongoClient = ConnectionManager.getMongo();
+		MongoDatabase database = ConnectionManager.getDb("test");
 		MongoCollection<Document> collection = database.getCollection("cafes");
 
 //		MongoCursor<Document> cursor = collection
@@ -34,32 +35,39 @@ public class Main {
 //			System.out.printf("City: %s | Population: %d | State:%s\n", city, pop, state);
 //		}
 
-		Document document1 = new Document("name", "Café Con Leche")
-				.append("contact",
-						new Document("phone", "228-555-0149").append("email", "cafeconleche@example.com")
-								.append("location", Arrays.asList(-73.92502, 40.8279556)))
-				.append("stars", 5).append("categories", Arrays.asList("Bakery", "Coffee", "Pastries"));
-		Document document2 = new Document("name", "Café Con Leche")
-				.append("contact",
-						new Document("phone", "228-555-0149").append("email", "cafeconleche@example.com")
-								.append("location", Arrays.asList(-73.92502, 40.8279556)))
-				.append("stars", 5).append("categories", Arrays.asList("Bakery", "Coffee", "Pastries"));
-		Document document3 = new Document("name", "Café Con Leche")
-				.append("contact",
-						new Document("phone", "228-555-0149").append("email", "cafeconleche@example.com")
-								.append("location", Arrays.asList(-73.92502, 40.8279556)))
-				.append("stars", 5).append("categories", Arrays.asList("Bakery", "Coffee", "Pastries"));
-
-		List<Document> docs = new ArrayList<>();
-		docs.add(document1);
-		docs.add(document2);
-		docs.add(document3);
-		collection.insertMany(docs);
+//		Document document1 = new Document("name", "Café Con Leche")
+//				.append("contact",
+//						new Document("phone", "228-555-0149").append("email", "cafeconleche@example.com")
+//								.append("location", Arrays.asList(-73.92502, 40.8279556)))
+//				.append("stars", 5).append("categories", Arrays.asList("Bakery", "Coffee", "Pastries"));
+//		Document document2 = new Document("name", "Café Con Leche")
+//				.append("contact",
+//						new Document("phone", "228-555-0149").append("email", "cafeconleche@example.com")
+//								.append("location", Arrays.asList(-73.92502, 40.8279556)))
+//				.append("stars", 5).append("categories", Arrays.asList("Bakery", "Coffee", "Pastries"));
+//		Document document3 = new Document("name", "Café Con Leche")
+//				.append("contact",
+//						new Document("phone", "228-555-0149").append("email", "cafeconleche@example.com")
+//								.append("location", Arrays.asList(-73.92502, 40.8279556)))
+//				.append("stars", 5).append("categories", Arrays.asList("Bakery", "Coffee", "Pastries"));
+//
+//		List<Document> docs = new ArrayList<>();
+//		docs.add(document1);
+//		docs.add(document2);
+//		docs.add(document3);
+//		collection.insertMany(docs);
 //		collection.insertOne(document);
 
 //		collection.deleteOne(Filters.eq("_id", new ObjectId("5dae97d42f01f961eba9fbf3")));
+		
+		collection.updateOne(Filters.eq("_id", new ObjectId("5daea1e19e6f5a44b247f6fe")), 
+				Updates.combine(
+						Updates.set("name", "Café new Leche"),
+						Updates.set("contact.phone", "233-111-0011"), 
+						Updates.set("stars", 5))
+				);
 
-		mongoClient.close();
+		ConnectionManager.close();
 
 	}
 
