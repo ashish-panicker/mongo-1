781a9260f88071bfe325100726809a742e7f9aab
ashish-panicker <ashish.s.panicker@gmail.com>
2019-10-22 12:01:42 +0530

    mongo 1 with create, remove, read

--- src/mongo1/Main.java
+++ src/mongo1/Main.java
@@ -1,26 +1,64 @@
 package mongo1;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.bson.Document;
+import org.bson.types.ObjectId;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
 		MongoDatabase database = mongoClient.getDatabase("test");
-		MongoCollection<Document> collection = database.getCollection("zips");	
-		
-		MongoCursor<Document> cursor =  collection.find().iterator();
-		while (cursor.hasNext()) {
-			System.out.println(cursor.next().toJson());
-		}
+		MongoCollection<Document> collection = database.getCollection("cafes");
+
+//		MongoCursor<Document> cursor = collection
+//				.find(Filters.nin("state",Arrays.asList("NY","TX")))
+//				.iterator();
+//		while (cursor.hasNext()) {
+//			Document d = (Document) cursor.next();
+//			String city = d.getString("city");
+//			int pop = d.getInteger("pop");
+//			String state = d.getString("state");
+//			System.out.printf("City: %s | Population: %d | State:%s\n", city, pop, state);
+//		}
+
+		Document document1 = new Document("name", "Café Con Leche")
+				.append("contact",
+						new Document("phone", "228-555-0149").append("email", "cafeconleche@example.com")
+								.append("location", Arrays.asList(-73.92502, 40.8279556)))
+				.append("stars", 5).append("categories", Arrays.asList("Bakery", "Coffee", "Pastries"));
+		Document document2 = new Document("name", "Café Con Leche")
+				.append("contact",
+						new Document("phone", "228-555-0149").append("email", "cafeconleche@example.com")
+								.append("location", Arrays.asList(-73.92502, 40.8279556)))
+				.append("stars", 5).append("categories", Arrays.asList("Bakery", "Coffee", "Pastries"));
+		Document document3 = new Document("name", "Café Con Leche")
+				.append("contact",
+						new Document("phone", "228-555-0149").append("email", "cafeconleche@example.com")
+								.append("location", Arrays.asList(-73.92502, 40.8279556)))
+				.append("stars", 5).append("categories", Arrays.asList("Bakery", "Coffee", "Pastries"));
+
+		List<Document> docs = new ArrayList<>();
+		docs.add(document1);
+		docs.add(document2);
+		docs.add(document3);
+		collection.insertMany(docs);
+//		collection.insertOne(document);
+
+//		collection.deleteOne(Filters.eq("_id", new ObjectId("5dae97d42f01f961eba9fbf3")));
+
 		mongoClient.close();
 
 	}
